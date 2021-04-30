0a2cddefca44966f9fbd7ffd8f35cc402862f020
Abhijeet <77497897+abhijeet20h@users.noreply.github.com>
2021-04-30 11:36:23 +0530

    Add files via upload

---
+++ 2.otherWays.js
@@ -0,0 +1,14 @@
+var things = new Array(3);
+
+things[0] = "Uma";
+things[1] = "Mahesh";
+things[2] = "Meka";
+things[3] = "jamie"; // we can still create thsi one
+
+console.log(things[0])
+console.log(things[1])
+console.log(things[2])
+console.log(things)
+
+
+
