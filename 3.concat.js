17d08d69e8811f02e831f31f1e0759a02b0c4cb6
Abhijeet <77497897+abhijeet20h@users.noreply.github.com>
2021-05-01 12:42:10 +0530

    Add files via upload

---
+++ 3.concat.js
@@ -0,0 +1,8 @@
+var fruits = new Array("Apple","Banana", "Citrus");
+var vegetables = new Array("Drumstick", "EggPlant", "FingerOnions")
+
+var getAllIetms1 = fruits.concat(vegetables); // if fruits to be displayed in the begining
+console.log(getAllIetms1);
+
+var getAllIetms2 = vegetables.concat(fruits); // if vegetables to be displayed in the begining
+console.log(getAllIetms2);
