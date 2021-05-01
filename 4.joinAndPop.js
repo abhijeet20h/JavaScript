76c870745fdc1a4400518eb16aafe25ee0f9469e
Abhijeet <77497897+abhijeet20h@users.noreply.github.com>
2021-05-01 12:42:29 +0530

    Add files via upload

---
+++ 4.joinAndPop.js
@@ -0,0 +1,14 @@
+// Using Join to get it as a string use join
+var movies1 = new Array("Avatar", "Good Will Hunting", "vanilla Sky", "Fight Club", "movie1");
+
+var movieString = movies1.join(",");
+
+console.log(movieString)
+
+// Suing pop to remove elements items from array from the end
+
+var movies2 = new Array("Avatar", "Good Will Hunting", "vanilla Sky", "Fight Club", "movie1");
+
+ movies2.pop()
+
+console.log(movies2.join(","))
